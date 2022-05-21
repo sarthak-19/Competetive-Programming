a6016d6c4451d7fbec56e05e67c2bd97362c2944
sarthak-19 <sarthak.8858@gmail.com>
2022-05-21 23:50:20 +0530

    Q2 of jobathon added

---
+++ GFG Jobathon 9/q2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws Exception
+    {
+        try
+        {
+            System.out.println("Hello World");
+            
+        }
+        catch(Exception e) {}
+    }
+}
